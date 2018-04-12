bb51554b90074546ebeb92f3ec99794dda23c5ce
AlejandraRamirez <ma.ramirez@uniandes.edu.co>
2018-04-11 21:22:30 -0500

    Ajustada funcion

---
+++ func.py
@@ -0,0 +1,80 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+#Info input by user en forma de lista
+#[0] nombre del .py - [1] nombre imagen - [2] ancho de gausiana
+inn = sys.argv
+name = inn[1]
+wd = float(inn[2])
+
+
+# covertir imagen del input a un array
+arr = plt.imread(name)
+
+#Dimensiones imagen
+#(y,x,capas)
+print(np.shape(arr))
+(YY, XX, capas) = np.shape(arr)
+print YY
+print XX
+print(np.shape(arr))
+
+#Funcion que define gausiana del suavizado
+#Param:coordenada x, y y el ancho, centrada en (cx,cy)
+#Return:punto de gausiana resultante
+def gauss(x,y,anc,cx,cy):
+    c = anc
+    #normalizacion
+    a = 1/float(c*c*2*np.pi)
+
+    ux = (x-cx)*(x-cx)
+    uy = (y-cy)*(y-cy)
+
+    ff = ux/float(2*c*c)
+    gg = uy/float(2*c*c)
+
+    ee = np.exp(-(ff+gg))
+    sol = a*ee
+    return sol
+
+#determinar centrado de gausiana en base a imagen
+#Param: dimensiones Y,X de la imagen
+#return: lista con centro de la imagen (cy,cx)
+def cen(Y,X):
+    sol = list()
+    cx = 0.0
+    cy = 0.0
+    if(Y%2 != 0):
+        cy = int((Y/2.0) + 1)
+    if(Y%2 == 0):
+        cy = int((Y/2.0))
+    if(X%2 != 0):
+        cx = int((X/2.0) + 1)
+    if(X%2 == 0):
+        cx = int((X/2.0))
+    sol.append(cy)
+    sol.append(cx)
+    return sol
+
+print cen(YY,XX)
+
+#Calcular centro de imagen
+cc = cen(YY,XX)
+
+#Array donde se guardara gausiana
+ga = np.zeros((YY,XX))
+#print ga
+
+#Generar gausiana en base a imagen
+for h in range(YY):
+    for j in range(XX):
+        ga[h][j] = gauss(h,j,wd,cc[0],cc[1])
+
+
+
+a = 5.0
+b = 2.0
+c = 3.0
+print int(a/b)
+print int(a/c)
---
+++ ga1.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+#Info input by user en forma de lista
+#[0] nombre del .py - [1] nombre imagen - [2] ancho de gausiana
+inn = sys.argv
+name = inn[1]
+wd = float(inn[2])
+
+
+# covertir imagen del input a un array
+arr = plt.imread(name)
+
+#Dimensiones imagen
+#(y,x,capas)
+print(np.shape(arr))
+(YY, XX, capas) = np.shape(arr)
+print YY
+print XX
+print(np.shape(arr))
+
+def gaussx(x,anc,cx):
+    c = anc
+    #normalizacion
+    a = 1/float(c*np.sqrt(2*np.pi))
+
+    ux = (x-cx)*(x-cx)
+    #uy = (y-cy)*(y-cy)
+
+    ff = ux/float(2*c*c)
+    #gg = uy/float(2*c*c)
+
+    ee = np.exp(-ff)
+    sol = a*ee
+    return sol
+
+def cen(Y,X):
+    sol = list()
+    cx = 0.0
+    cy = 0.0
+    if(Y%2 != 0):
+        cy = int((Y/2.0) + 1)
+    if(Y%2 == 0):
+        cy = int((Y/2.0))
+    if(X%2 != 0):
+        cx = int((X/2.0) + 1)
+    if(X%2 == 0):
+        cx = int((X/2.0))
+    sol.append(cy)
+    sol.append(cx)
+    return sol
+
+#Calcular centro de imagen
+cc = cen(YY,XX)
+print cc
+
+xx = np.linspace(0,YY-1,YY)
+yyy = np.linspace(0,YY-1,YY)
+
+for i in xx:
+    yyy[i] = gaussx(i,wd,cc[0])
+
+plt.plot(xx,yyy)
+plt.show()
